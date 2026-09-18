099ded1508a408d97b017f97162abe0a8eb9b3b5
fat4-alt <346881691@qq.com>
2026-09-18 08:35:48 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -105,6 +105,20 @@ define Build/cetron-header
 	rm $@.tmp
 endef
 
+define Device/fzs_5gcpe-p5
+  DEVICE_VENDOR := FZS
+  DEVICE_MODEL := 5GCPE P5
+  DEVICE_DTS := mt7981b-fzs-5gcpe-p5
+  DEVICE_DTS_DIR := ../dts
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_IN_UBI := 1
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += fzs_5gcpe-p5
+
 define Device/abt_asr3000
   DEVICE_VENDOR := ABT
   DEVICE_MODEL := ASR3000
